e4257974896c433416056c9216c04519466d61ea
amit0961 <amitsaha5bd@gmail.com>
2022-10-25 12:42:59 +0600

    Lumenapi setup

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/get', function () use ($router) {
+    return "I am get";
+    // return $router->app->version();
+});
+$router->post('/post', function () use ($router) {
+    return "I am post";
+    // return $router->app->version();
+});
+$router->put('/put', function () use ($router) {
+    return "I am put";
+    // return $router->app->version();
+});
+$router->delete('/delete', function () use ($router) {
+    return "I am delete";
+    // return $router->app->version();
+});
+
+$router->post('/amit', function(){
+    return "I am Amit Saha";
+});
